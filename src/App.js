8b1c12a5ea17e2862393bcad0e0dcb8e3cd17ea0
Grey <bloodecho001@gmail.com>
2024-06-02 01:36:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div>
-      <h1>Hello, GitHub Actions!</h1>
-    </div>
-  );
-}
-
-export default App;
+document.getElementById('root').innerHTML = `
+  <div>
+    <h1>Hello, GitHub Actions!</h1>
+  </div>
+`;
